a4b5c9a767990c4f19bb578a6a083fbadec40442
maase_alvin <maasealvin@gmail.com>
2023-05-02 15:10:49 +0300

    added new tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,12 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """
+    Helper function to create a user
+    """
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTests(TestCase):
     """
     Test for models
@@ -72,3 +78,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,18 +1,24 @@
 """
 Views for the recipe APIs.
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets, 
+    mixins
+    )
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """
-    View for manage recipe APIs
+    View for managing recipe APIs
     """
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -23,11 +29,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
-    # def perform_create(self, serializer):
-    #     """
-    #     Create a new recipe
-    #     """
-    #     serializer.save(user=self.request.user
     def get_serializer_class(self):
         """
         Return appropriate serializer class
@@ -36,3 +37,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
         
         return self.serializer_class
+    
+    def perform_create(self, serializer):
+        """
+        Create a new recipe
+        """
+        serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin, 
+                 mixins.ListModelMixin, 
+                 viewsets.GenericViewSet):
+    """
+  Manage tags in the database
+    """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        """
+        Return objects for the current authenticated user only
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+    def perform_create(self, serializer):
+        """
+        Create a new tag
+        """
+        # serializer.save(user=self.request.user
